2b268431687a97e6ed16d9f464e9b21cac11d15b
Felix Gutmann <gutmann@traum-ferienwohnungen.de>
2020-03-22 14:57:19 +0100

    Change indentations to spaces

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,15 @@ IFS=$'\n\t'
 
 env | grep ^KAFKA_CFG_ | sed 's/^KAFKA_CFG_//' | while IFS='=' read -r n v; do
 
-	key=$(echo $n | awk '{print tolower($0)}' | sed 's/\_/./g')
+    key=$(echo $n | awk '{print tolower($0)}' | sed 's/\_/./g')
 
-	if [[ ! -z $(grep "$key=" $KAFKA_RUNTIME_HOME/config/server.properties) ]]; then
+    if [[ ! -z $(grep "$key=" $KAFKA_RUNTIME_HOME/config/server.properties) ]]; then
     
-    	sed -i "/$key/d" $KAFKA_RUNTIME_HOME/config/server.properties
-	
-	fi 
+        sed -i "/$key/d" $KAFKA_RUNTIME_HOME/config/server.properties
+    
+    fi 
 
-	echo "$key=$v" >> $KAFKA_RUNTIME_HOME/config/server.properties 
+    echo "$key=$v" >> $KAFKA_RUNTIME_HOME/config/server.properties 
 
 done
 
